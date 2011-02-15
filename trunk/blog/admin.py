694fd1cb8fcac8c3a0528b7944fc9e0963e590a6
matheus.se@gmail.com <matheus.se@gmail.com@62c59049-f721-ff75-3e79-0880076c9452>
2011-02-15 12:39:51 +0000

    deletando tudo

--- trunk/blog/admin.py
+++
@@ -1,6 +0,0 @@
-from models import Artigo, Pessoa
-from django.contrib import admin
-
-
-admin.site.register(Artigo)
-admin.site.register(Pessoa)
--- trunk/teste.py
+++
@@ -1,4 +0,0 @@
-import os
-PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-print PROJECT_ROOT_PATH
