48c34a0165af34c046b65f986aff9ac5328d6dde
matheus.se@gmail.com <matheus.se@gmail.com@62c59049-f721-ff75-3e79-0880076c9452>
2011-02-16 11:26:38 +0000

    Adicionando o projeto "teste"

---
+++ trunk/projetos/teste/blog/admin.py
@@ -0,0 +1,6 @@
+from models import Artigo, Pessoa
+from django.contrib import admin
+
+
+admin.site.register(Artigo)
+admin.site.register(Pessoa)
---
+++ trunk/projetos/teste/teste.py
@@ -0,0 +1,4 @@
+import os
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+print PROJECT_ROOT_PATH
